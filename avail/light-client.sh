998eb23669395a182642de8037223ab0e97cb1a9
thucvn <thucvn@vnpt.vn>
2024-04-03 16:49:06 +0700

    * remove choice

--- avail/light-client.sh
+++ avail/light-client.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 clear
 
-
-
 echo "===========Avail light client Install Easy======= " && sleep 1
 
-read -p "Do you update and install curl? (y/n): " choice
-
-if [ "$choice" == "y" ]; then
-    sudo apt update && sudo apt upgrade -y
-    sudo apt install curl
-fi
 sudo tee /etc/systemd/system/avl-light.service > /dev/null <<EOF
 [Unit]
 Description=Avail light client Daemon
